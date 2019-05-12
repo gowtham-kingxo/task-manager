31893be3cab636df2ae4bbc8be68563adc0f1955
gowthamgenupur <genupur.g@husky.neu.edu>
2019-05-12 13:45:15 +0530

    Building RESTful APIs

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 mongoose.connect('mongodb://127.0.0.1/task-manager', {
     useNewUrlParser: true,
@@ -7,18 +8,78 @@ mongoose.connect('mongodb://127.0.0.1/task-manager', {
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     }, 
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if(!validator.isEmail(value)) {
+                throw new Error('Email is invalid ')
+            }
+        }
+
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 6,
+        trim: true,
+        validate(value) {
+            if(value.toLowerCase().includes("password")) {
+                throw new Error('Please do not inlcude the phrase "password" in your password.')
+            }
+        } 
+    },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if(value < 0) {
+                throw new Error('Age must be a positive number.')
+            }
+        }
     }
 })
 
+//testing
+
+const me = new User({
+    name: '   mike   ',
+    email: 'Mike@HOTMAIL.in',
+    password: 'mylight'
+})
+
+// me.save().then(() => {
+//     console.log(me)
+// }).catch((error) => {
+//     console.log('Error!', error)
+// })
+
 const Task = mongoose.model('Task', {
     description: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     }, 
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     }
+})
+
+
+const task1 = new Task({
+   completed: false
+
+})
+
+task1.save().then(() => {
+    console.log('task1')
+}).catch(error => {
+    console.log(error)
 })
