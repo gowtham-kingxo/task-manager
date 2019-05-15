2373aefdafd48b36b0ac64ae6946fdf6d6ad1e07
gowthamgenupur <genupur.g@husky.neu.edu>
2019-05-15 23:26:40 +0530

    Refactored update task with auth

--- src/routers/task.js
+++ src/routers/task.js
@@ -7,8 +7,9 @@ const router = new express.Router()
 router.get('/tasks', auth, async (req, res) => {
 
     try {
-        const tasks = await Task.find({owner: req.user._id})
-        res.status(201).send(tasks)
+        //or const tasks = await Task.find({owner: req.user._id})
+        await req.user.populate('tasks').execPopulate()
+        res.status(200).send(req.user.tasks)
     } catch (error) {
         res.status(500).send(error)
     }
@@ -36,7 +37,7 @@ router.get('/tasks/:id', auth, async (req, res) => {
 
 
 //RESTful service for updating a task
-router.patch('/tasks/:id', async (req, res) => {
+router.patch('/tasks/:id', auth, async (req, res) => {
 
     //Checks validity of the update
     const updates = Object.keys(req.body)
@@ -50,7 +51,7 @@ router.patch('/tasks/:id', async (req, res) => {
     const _id = req.params.id
 
     try {
-        const task = await Task.findById(_id)
+        const task = await Task.findOne({ _id, owner: req.user._id})
         if(!task) {
             return res.status(404).send()
         }
